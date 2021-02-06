0afe638d12a73366036fe36fde4dc93b22602704
Fdeligans <fabien.deligans@ingeli.fr>
2021-02-06 21:00:01 +0100

    ok

---
+++ PostgreSQLBlazorApp/PostgreSQLBlazorApp/Migrations/20210206195822_init.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace PostgreSQLBlazorApp.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Famille",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NomDeFamille = table.Column<string>(nullable: true),
+                    Qf = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Famille", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enfant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(nullable: true),
+                    Prenom = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    FamilleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enfant", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Enfant_Famille_FamilleId",
+                        column: x => x.FamilleId,
+                        principalTable: "Famille",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(nullable: true),
+                    Prenom = table.Column<string>(nullable: true),
+                    Adresse = table.Column<string>(nullable: true),
+                    Cp = table.Column<string>(nullable: true),
+                    Ville = table.Column<string>(nullable: true),
+                    Mail = table.Column<string>(nullable: true),
+                    Tel = table.Column<string>(nullable: true),
+                    FamilleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Parent_Famille_FamilleId",
+                        column: x => x.FamilleId,
+                        principalTable: "Famille",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enfant_FamilleId",
+                table: "Enfant",
+                column: "FamilleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parent_FamilleId",
+                table: "Parent",
+                column: "FamilleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enfant");
+
+            migrationBuilder.DropTable(
+                name: "Parent");
+
+            migrationBuilder.DropTable(
+                name: "Famille");
+        }
+    }
+}
--- PostgreSQLBlazorApp/PostgreSQLBlazorApp/Startup.cs
+++ PostgreSQLBlazorApp/PostgreSQLBlazorApp/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace PostgreSQLBlazorApp
 {
@@ -29,6 +30,9 @@ namespace PostgreSQLBlazorApp
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddDbContext<Context>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
